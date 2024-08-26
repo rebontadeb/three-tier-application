6fee7bcbc139e70c3f2aa7d92698d6c9617e779f
Sayantan <sayantandeb10@gmail.com>
2024-08-26 21:40:00 +0530

    Added database in create table

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,2 +1,2 @@
-create table donorDemographic (donorName varchar(20), pincode varchar(6),phonenumber varchar(10) );
-ALTER TABLE donorDemographic ADD CONSTRAINT UC_donorDemographic UNIQUE (phonenumber);
+create table donors.donorDemographic (donorName varchar(20), pincode varchar(6),phonenumber varchar(10) );
+ALTER TABLE donors.donorDemographic ADD CONSTRAINT UC_donorDemographic UNIQUE (phonenumber);
